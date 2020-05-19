dbbc5d36c7f22328b849ba5bf4510b8995a5ecc7
mlaskowski17 <marcin.ladesign@gmail.com>
2020-05-19 18:57:52 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # TeachableMachine
 play with Teachable Machine sample code
+
+
+## Goal
+Detect whether I am drinking a coffee or not.
---
+++ syndicai.py
@@ -0,0 +1,51 @@
+import tensorflow.keras
+from PIL import Image, ImageOps
+import numpy as np
+
+
+def run(input_data, output_data):
+    """
+    Face recognition algorithm
+
+    :param input_data: path for the directory from which function will take file.
+                In this case it's and image (.jpg, .jpeg, .png)
+    :param output_data: none
+    :return: a list with the probabilites
+    """
+    for input_file in input_data: 
+
+        # Disable scientific notation for clarity
+        np.set_printoptions(suppress=True)
+
+        # Load the model
+        model = tensorflow.keras.models.load_model('keras_model.h5')
+
+        # Create the array of the right shape to feed into the keras model
+        # The 'length' or number of images you can put into the array is
+        # determined by the first position in the shape tuple, in this case 1.
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+        # Replace this with the path to your image
+        image = Image.open(input_file)
+
+        #resize the image to a 224x224 with the same strategy as in TM2:
+        #resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+        #turn the image into a numpy array
+        image_array = np.asarray(image)
+
+        # display the resized image
+        #image.show()
+
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+        # Load the image into the array
+        data[0] = normalized_image_array
+
+        # run the inference
+        prediction = model.predict(data)
+
+        return {"without_mug": prediction[0][0], "with_mug": prediction[0][1]}
